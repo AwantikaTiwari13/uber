9faee3b9f8d00de8b1f4ae852b028056870dcd64
awantika <tiwariawantika13@gmail.com>
2024-12-22 22:35:40 +0530

    user authentication done

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.urlencoded({extended: true}));
 

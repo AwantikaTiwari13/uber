597d3506aa37143cf85df4be713a286dc6314369
awantika <tiwariawantika13@gmail.com>
2024-12-27 20:47:28 +0530

    created suggestions, getCoordinates, distanceTime enpoints with google maps

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
-
+const mapsRoutes = require('./routes/maps.routes');
 
 app.use(cors());
 app.use(express.json());
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
-
+app.use('/maps', mapsRoutes)
 
 module.exports = app;
 

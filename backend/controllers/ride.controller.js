0528a29552686d27bb2aed016481c53abe75e5eb
awantika <tiwariawantika13@gmail.com>
2025-07-27 12:01:24 +0530

    changes in starting page

--- backend/controllers/ride.controller.js
+++ backend/controllers/ride.controller.js
@@ -24,7 +24,6 @@ module.exports.createRide = async (req, res) => {
 
     const pickupCoordinates = await mapService.getAddressCoordinate(pickup);
 
-    console.log("Pickup Coordinates:", pickupCoordinates);
     const captainsInRadius = await mapService.getCaptainsInTheRadius(
       pickupCoordinates.ltd,
       pickupCoordinates.lng,
@@ -32,14 +31,17 @@ module.exports.createRide = async (req, res) => {
     );
     ride.otp = "";
 
-    const rideWithUser = await rideModel.findOne({ _id: ride._id}).populate('user')
+    const rideWithUser = await rideModel
+      .findOne({ _id: ride._id })
+      .populate("user");
 
     captainsInRadius.map((captain) => {
       sendMessageToSocketId(captain.socketId, {
         event: "new-ride",
-        data: rideWithUser
+        data: rideWithUser,
+      });
     });
-  })} catch (err) {
+  } catch (err) {
     console.error(err);
     res.status(500).json({ message: err.message });
   }
@@ -62,76 +64,76 @@ module.exports.getFare = async (req, res) => {
   }
 };
 
-
 module.exports.confirmRide = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const { rideId } = req.body;
 
   try {
-      const ride = await rideService.confirmRide({ rideId, captain: req.captain });
+    const ride = await rideService.confirmRide({
+      rideId,
+      captain: req.captain,
+    });
 
-      sendMessageToSocketId(ride.user.socketId, {
-          event: 'ride-confirmed',
-          data: ride
-      })
+    sendMessageToSocketId(ride.user.socketId, {
+      event: "ride-confirmed",
+      data: ride,
+    });
 
-      return res.status(200).json(ride);
+    return res.status(200).json(ride);
   } catch (err) {
-
-      console.log(err);
-      return res.status(500).json({ message: err.message });
+    console.log(err);
+    return res.status(500).json({ message: err.message });
   }
-}
+};
 
 module.exports.startRide = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const { rideId, otp } = req.query;
 
   try {
-      const ride = await rideService.startRide({ rideId, otp, captain: req.captain });
+    const ride = await rideService.startRide({
+      rideId,
+      otp,
+      captain: req.captain,
+    });
 
-      console.log(ride);
+    sendMessageToSocketId(ride.user.socketId, {
+      event: "ride-started",
+      data: ride,
+    });
 
-      sendMessageToSocketId(ride.user.socketId, {
-          event: 'ride-started',
-          data: ride
-      })
-
-      return res.status(200).json(ride);
+    return res.status(200).json(ride);
   } catch (err) {
-      return res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
-}
-
+};
 
 module.exports.endRide = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const { rideId } = req.body;
 
   try {
-      const ride = await rideService.endRide({ rideId, captain: req.captain });
+    const ride = await rideService.endRide({ rideId, captain: req.captain });
 
-      sendMessageToSocketId(ride.user.socketId, {
-          event: 'ride-ended',
-          data: ride
-      })
+    sendMessageToSocketId(ride.user.socketId, {
+      event: "ride-ended",
+      data: ride,
+    });
 
-
-
-      return res.status(200).json(ride);
+    return res.status(200).json(ride);
   } catch (err) {
-      return res.status(500).json({ message: err.message });
-  } 
-}
+    return res.status(500).json({ message: err.message });
+  }
+};
